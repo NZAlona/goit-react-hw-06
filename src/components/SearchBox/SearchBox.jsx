58b1618765b8f9299c8f896338c830dfa918edf0
NZAlona <amakoveychenko@gmail.com>
2024-03-16 15:38:46 +0200

    Adds redux persist libr to save contacts

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,13 @@
 import { useId } from 'react';
 import css from './SearchBox.module.css';
-// import { selectNameFilter } from '../../redux/filtersSlice';
-// import { useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filtersSlice';
 import { useDispatch } from 'react-redux';
 
 export default function SearchBox() {
-  // const value = useSelector(selectNameFilter);
-
-  // console.log(value);
   const dispatch = useDispatch();
+
   const inputField = useId();
+
   return (
     <>
       <label htmlFor={inputField} className={css.labelText}>
@@ -20,7 +17,6 @@ export default function SearchBox() {
       <input
         type="text"
         id={inputField}
-        // value={value}
         onChange={e => dispatch(changeFilter(e.target.value))}
         className={css.inputField}
       />
